65997e5fd80e3989cddb081c564dbcee0f386411
soumya <soumya.chakraborty@hoichoi.tv>
2024-02-29 16:38:20 +0530

    initial setup is done

--- week-7/assignment-1/index.js
+++ week-7/assignment-1/index.js
@@ -1,19 +1,20 @@
-
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const port = 3000;
-const authRoutes = require("./routes/auth");
-const todoRoutes = require("./routes/todo");
-const cors = require("cors");
+const authRoutes = require('./routes/auth');
+const todoRoutes = require('./routes/todo');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRoutes);
-app.use("/todo", todoRoutes);
+app.use('/auth', authRoutes);
+app.use('/todo', todoRoutes);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
 
-mongoose.connect('mongodb://localhost:27017/courses', { dbName: "courses" });
+mongoose.connect('mongodb+srv://soumya:soumya@cluster0.m7m8wqs.mongodb.net/', {
+	dbName: 'courses'
+});
